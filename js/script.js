b7feb4eb37411d3d3b4f4e67349b4f5a6bbdbdb7
Owusu-Desmond <desmond0406114009@gmail.com>
2022-06-13 10:38:14 +0000

    Close mobile menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 const hamburger = document.querySelector('.btn-nav');
 const navMenu = document.querySelector('.nav-links');
+const closeBtn = document.querySelector('.close-btn');
 
-hamburger.addEventListener('click', ()=>{
-    navMenu.classList.toggle('mobile-menu')
-  });
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('mobile-menu')
+});
+
+closeBtn.addEventListener('click', () => {
+  navMenu.classList.toggle('mobile-menu')
+});
+
