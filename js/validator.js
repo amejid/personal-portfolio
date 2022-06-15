930ba1dac229f8bd09631c0a5e79665c9101ca58
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-06-15 17:16:54 +0300

    Add event listener block

--- js/validator.js
+++ js/validator.js
@@ -3,3 +3,14 @@ const valResult = document.querySelector('.val-result');
 const [email] = contactForm.elements;
 
 const isInLowerCase = (str) => str === String(str).toLowerCase();
+
+contactForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const emailText = email.value;
+  if (!isInLowerCase(emailText)) {
+    valResult.textContent = 'Email should be in lowercase';
+  } else {
+    contactForm.submit();
+  }
+});
