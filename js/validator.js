23ecd410a42ac4d8c1b859a271c9f02d6f3706d9
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-06-15 17:29:55 +0300

    Refactor code for re-use

--- js/validator.js
+++ js/validator.js
@@ -1,16 +1,26 @@
 const contactForm = document.getElementById('contact-form');
+const contactFormDesk = document.getElementById('contact-form-big');
+
 const valResult = document.querySelector('.val-result');
+const valResultDesk = document.querySelector('.val-result-big');
+
 const [email] = contactForm.elements;
+const emailDesk = contactFormDesk.elements.email;
 
 const isInLowerCase = (str) => str === String(str).toLowerCase();
 
-contactForm.addEventListener('submit', (e) => {
-  e.preventDefault();
+const validateForm = (form, emailInput, msgOutput) => {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-  const emailText = email.value;
-  if (!isInLowerCase(emailText)) {
-    valResult.textContent = 'Email should be in lowercase';
-  } else {
-    contactForm.submit();
-  }
-});
+    const emailText = emailInput.value;
+    if (!isInLowerCase(emailText)) {
+      msgOutput.textContent = 'Email should be in lowercase';
+    } else {
+      form.submit();
+    }
+  });
+};
+
+validateForm(contactForm, email, valResult);
+validateForm(contactFormDesk, emailDesk, valResultDesk);
